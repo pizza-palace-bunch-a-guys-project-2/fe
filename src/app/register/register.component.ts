4f3818e6c7c94403b4c1010b06416b6058ef82c4
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-10-25 14:56:30 -0400

    Basic register user logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { User } from '../services/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  signUpGroup = new FormGroup({
+    userName: new FormControl(''),
+    userPassword: new FormControl(''),
+    userEmail: new FormControl(''),
+    userFirstName: new FormControl(''),
+    userLastName: new FormControl('')
+  });
+
+  constructor(private userServ:UserService) { }
+
+  signUpUser(signUpGroup: FormGroup) {
+    const userName:string = signUpGroup.get('userName')!.value;
+    const userPassword:string = signUpGroup.get('userPassword')!.value;
+    const userEmail:string = signUpGroup.get('userEmail')!.value;
+    const userFirstName:string = signUpGroup.get('userFirstName')!.value;
+    const userLastName:string = signUpGroup.get('userLastName')!.value;
+    const userRoleId:number = 1;
+
+    const user = new User(userName, userPassword, userEmail, userFirstName, userLastName, userRoleId);
+
+    this.userServ.signUpUser(user).subscribe(
+      resp => {
+        console.log("Response: ", resp);
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
