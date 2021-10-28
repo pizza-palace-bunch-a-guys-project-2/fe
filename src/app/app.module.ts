24ef8fcae3d24fca6e439747939b1b81643edc26
MJad98 <91090176+MJad98@users.noreply.github.com>
2021-10-27 20:35:19 -0500

    Wiring attempt
    help

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import "@angular/compiler";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from './services/user.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
@@ -23,10 +26,12 @@ import { UserService } from './services/user.service';
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([
+      {path: 'checkout', component: CheckoutComponent},
       {path: 'login', component: LoginComponent},
       {path: 'register', component: RegisterComponent},
       {path: '**', redirectTo: 'login'}
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
---
+++ src/app/services/menuitem.ts
@@ -0,0 +1,16 @@
+export interface Item {
+   
+type:string,
+modifiers:string[],
+price:number,
+name:string,
+image:string,
+description:string
+    
+}
+
+export interface Data {
+    username: '',
+    items: []
+}
+
