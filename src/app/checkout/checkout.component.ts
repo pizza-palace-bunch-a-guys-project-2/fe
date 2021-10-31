1e10cdd4f9f85e23b4ca4ddfde52c8a4d4a5ea5c
MJad98 <91090176+MJad98@users.noreply.github.com>
2021-10-30 21:05:06 -0500

    saturdaysuffering

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Order } from './order';
+import { CheckoutService } from './checkout.service';
 //import { MenuItemService } from '../services/menu-item.service';
 declare var AddressFinder: any;
 @Component({
@@ -7,46 +9,103 @@ declare var AddressFinder: any;
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
 
   hasOrder = false;
-  //userInfo:string; this will be the user object, and include fields like
-  //username, name, email, phone number, address. 
+
+
+
   paymentGroup = new FormGroup({
     card_number: new FormControl(''),
     cardholder: new FormControl(''),
     cardexpiration: new FormControl(''),
     security_code: new FormControl(''),
     billingzip: new FormControl('')
-    })
- 
+  });
+
+  addressGroup = new FormGroup({
+    street_name: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zipcode: new FormControl('')
+  });
+
+
   pizzaPic = "https://media.istockphoto.com/photos/cheesy-pepperoni-pizza-picture-id938742222?k=20&m=938742222&s=612x612&w=0&h=X5AlEERlt4h86X7U7vlGz3bDaDDGQl4C3MuU99u2ZwQ=";
-  userInfo:Object[];
-  itemList:String[];
+  userInfo: Object[];
+  items: String = "Cheese Pizza, Breadsticks";
+  total: number = 25;
+  address: String = "";
+  paymentDetails: String = "";
+  userId: number = 1;
   
-  constructor() {
-      // this.userInfo = cServ.userInfo;
-      // this.itemList = cServ.itemList;   private cServ:MenuItemService
-   }
 
+  constructor(private cServ:CheckoutService) {
+    // this.userInfo = cServ.userInfo;
+    // this.itemList = cServ.itemList;   private cServ:MenuItemService
+  }
+  ngOnInit(): void {
+    
+  }
 
+  createOrder() {
+    let order = new Order(this.items, this.getPaymentDetails(this.paymentGroup), this.getAddress(this.addressGroup), this.total, this.userId)
+    console.log(order);
+    
+    this.submitOrder(order);
+  }
 
-  getPaymentDetails(paymentGroup:FormGroup){
-    if(paymentGroup.get('card_number').value){
-      console.log('form input field card_number has value ' + paymentGroup.get('card_number').value)
-      
-    }
-      if(paymentGroup.get('cardholder').value){
-        console.log('form input field cardholder has value ' + paymentGroup.get('cardholder').value)
-    }
-    if(paymentGroup.get('cardexpiration').value){
-      console.log('form input field cardexpiration has value ' + paymentGroup.get('cardexpiration').value)
-    }
-    if(paymentGroup.get('security_code').value){
-      console.log('form input field security_code has value ' + paymentGroup.get('security_code').value)
-    }
-    if(paymentGroup.get('billingzip').value){
-      console.log('form input field billingzip has value ' + paymentGroup.get('billingzip').value)
-    }
-}
+  public submitOrder(order){
+    console.log(order);
+    let stringOrder = JSON.stringify(order);
+    this.cServ.insertOrder(stringOrder).subscribe(
+      response => {
+        console.log(response);
+      },
+      error =>{
+        console.warn("there was an error" + error);
+      }
+    )
+  }
+
+  getPaymentDetails(paymentGroup: FormGroup) {
+    let cardNumber = paymentGroup.get('card_number').value;
+    let cardHolder = paymentGroup.get('cardholder').value;
+    let cardExpiration = paymentGroup.get('cardexpiration').value;
+    let securityCode = paymentGroup.get('security_code').value;
+    let billingZip = paymentGroup.get('billingzip').value;
+
+    this.paymentDetails = "";
+
+    this.paymentDetails += cardNumber + ', ';
+    this.paymentDetails += cardHolder + ', ';
+    this.paymentDetails += cardExpiration + ', ';
+    this.paymentDetails += securityCode + ', ';
+    this.paymentDetails += billingZip;
+    console.log(this.paymentDetails);
+
+    return this.paymentDetails;
+
+  }
+
+  getAddress(addressGroup: FormGroup) {
+    let streetName = addressGroup.get('street_name').value;
+    let city = addressGroup.get('city').value;
+    let state = addressGroup.get('state').value;
+    let zipcode = addressGroup.get('zipcode').value;
+
+    this.address = "";
+
+    this.address += streetName + ', ';
+    this.address += city + ', ';
+    this.address += state + ', ';
+    this.address += zipcode;
+
+    console.log(this.address);
+
+    return this.address;
+  }
+
+  
+
 }
--- src/app/checkout/order.ts
+++ src/app/checkout/order.ts
@@ -1,6 +1,6 @@
 export class Order {
 
-    constructor(public items:String[], public paymentDetails:String[], 
+    constructor(public items:String, public paymentDetails:String, 
         public address:String, public total:number, public userId:number) {}
 
 }
