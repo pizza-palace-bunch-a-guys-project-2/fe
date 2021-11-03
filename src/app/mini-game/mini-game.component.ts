0b8089fc557fc68b16efc836d841524b73b02996
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-11-03 00:35:17 -0400

    Adding my mini game controls desc

--- src/app/mini-game/mini-game.component.ts
+++ src/app/mini-game/mini-game.component.ts
@@ -5,6 +5,7 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   selector: 'ngbd-modal-content',
   template: `
     <div class="modal-header">
+      <p><strong>Controls: to move hit arrows right -> and left <-; to jump hit "space".</strong><p>
       <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
         <span aria-hidden="true">&times;</span>
       </button>
